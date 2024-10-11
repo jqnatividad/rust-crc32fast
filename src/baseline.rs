a8ab9c33ba4abdaca886e8ab67bcd6b7ba3121f6
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2024-10-11 11:24:38 -0400

    refactor update_fast_size to process 128 bytes at a time from 64 bytes for each iteration of the

--- src/baseline.rs
+++ src/baseline.rs
@@ -29,30 +29,31 @@ impl State {
 }
 
 pub fn update_fast_16(prev: u32, mut buf: &[u8]) -> u32 {
-    const UNROLL: usize = 4;
+    const UNROLL: usize = 8;
     const BYTES_AT_ONCE: usize = 16 * UNROLL;
 
     let mut crc = !prev;
 
     while buf.len() >= BYTES_AT_ONCE {
-        for _ in 0..UNROLL {
-            crc = CRC32_TABLE[0x0][buf[0xf] as usize]
-                ^ CRC32_TABLE[0x1][buf[0xe] as usize]
-                ^ CRC32_TABLE[0x2][buf[0xd] as usize]
-                ^ CRC32_TABLE[0x3][buf[0xc] as usize]
-                ^ CRC32_TABLE[0x4][buf[0xb] as usize]
-                ^ CRC32_TABLE[0x5][buf[0xa] as usize]
-                ^ CRC32_TABLE[0x6][buf[0x9] as usize]
-                ^ CRC32_TABLE[0x7][buf[0x8] as usize]
-                ^ CRC32_TABLE[0x8][buf[0x7] as usize]
-                ^ CRC32_TABLE[0x9][buf[0x6] as usize]
-                ^ CRC32_TABLE[0xa][buf[0x5] as usize]
-                ^ CRC32_TABLE[0xb][buf[0x4] as usize]
-                ^ CRC32_TABLE[0xc][buf[0x3] as usize ^ ((crc >> 0x18) & 0xFF) as usize]
-                ^ CRC32_TABLE[0xd][buf[0x2] as usize ^ ((crc >> 0x10) & 0xFF) as usize]
-                ^ CRC32_TABLE[0xe][buf[0x1] as usize ^ ((crc >> 0x08) & 0xFF) as usize]
-                ^ CRC32_TABLE[0xf][buf[0x0] as usize ^ (crc & 0xFF) as usize];
-            buf = &buf[16..];
+        for i in 0..UNROLL {
+            let offset = i * 16;
+            crc = CRC32_TABLE[0x0][buf[offset + 0xf] as usize]
+                ^ CRC32_TABLE[0x1][buf[offset + 0xe] as usize]
+                ^ CRC32_TABLE[0x2][buf[offset + 0xd] as usize]
+                ^ CRC32_TABLE[0x3][buf[offset + 0xc] as usize]
+                ^ CRC32_TABLE[0x4][buf[offset + 0xb] as usize]
+                ^ CRC32_TABLE[0x5][buf[offset + 0xa] as usize]
+                ^ CRC32_TABLE[0x6][buf[offset + 0x9] as usize]
+                ^ CRC32_TABLE[0x7][buf[offset + 0x8] as usize]
+                ^ CRC32_TABLE[0x8][buf[offset + 0x7] as usize]
+                ^ CRC32_TABLE[0x9][buf[offset + 0x6] as usize]
+                ^ CRC32_TABLE[0xa][buf[offset + 0x5] as usize]
+                ^ CRC32_TABLE[0xb][buf[offset + 0x4] as usize]
+                ^ CRC32_TABLE[0xc][buf[offset + 0x3] as usize ^ ((crc >> 0x18) & 0xFF) as usize]
+                ^ CRC32_TABLE[0xd][buf[offset + 0x2] as usize ^ ((crc >> 0x10) & 0xFF) as usize]
+                ^ CRC32_TABLE[0xe][buf[offset + 0x1] as usize ^ ((crc >> 0x08) & 0xFF) as usize]
+                ^ CRC32_TABLE[0xf][buf[offset + 0x0] as usize ^ (crc & 0xFF) as usize];
+            buf = &buf[BYTES_AT_ONCE..];
         }
     }
 
